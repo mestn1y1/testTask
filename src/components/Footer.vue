85e0fa75a14f1e397052aefe5b2c9de6976ea7b6
Serhii Tyron <mestn1y@hotmail.com>
2025-02-08 15:46:46 +0200

    change fontSize in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="footer">
-    <p>&copy; 2025 Movie Cluster. Усі права захищено.</p>
+    <p class="footer-text">&copy; 2025 Movie Cluster. Усі права захищено.</p>
     <ul class="footer-links">
       <li><router-link to="/">Політика конфіденційності</router-link></li>
       <li>
@@ -42,11 +42,22 @@ export default {
   text-decoration: underline;
 }
 
-@media screen and (min-width: 374px) {
+@media screen and (min-width: 320px) and (max-width: 474px) {
+  .footer-text,
+  .footer-links {
+    font-size: 14px;
+  }
+}
+@media screen and (min-width: 475px) {
   .footer-links {
     flex-direction: row;
     justify-content: center;
     gap: 12px;
+  }
+
+  .footer-text,
+  .footer-links {
+    font-size: 16px;
   }
 }
 </style>
